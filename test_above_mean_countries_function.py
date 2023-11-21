c6c082b6fdbd84d5b3dd0ce8e9ac9e123b66eb27
eli <eli.kajbaf@gmail.com>
2023-11-21 08:21:09 -0330

    test function for above mean countries

---
+++ test_above_mean_countries_function.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pytest
+from above_mean_countries_function import calculate_countries_above_mean_count
+
+# Mock DataFrame for testing
+data = {
+    'year': [2000, 2001, 2002, 2003, 2004, 2005],
+    'country': ['A', 'B', 'C', 'D', 'E', 'F'],
+    'gsd_index_score': [10, 15, 12, 18, 38, 39],
+    'goal_1_score': [10, 15, 12, 18, 38, 39],
+    'goal_2_score': [5, 8, 6, 9, 40, 41],
+    'goal_3_score': [20, 18, 22, 30, 89, 90]
+}
+
+df = pd.DataFrame(data)
+
+
+def test_calculate_countries_above_mean_count(tmp_path):
+    # A directory to create the output CSV file
+    tmp_output_path = tmp_path / 'test_above_mean_count.csv'
+
+    # Call the function with the mock DataFrame and the temporary output path
+    result_df = calculate_countries_above_mean_count(df, output_csv_filename=str(tmp_output_path))
+
+    # Check the values in the DataFrame
+    assert result_df.loc[result_df['Goal'] == 'goal_1_score', 'Num_Countries_Above_Mean'].values[0] == 2
+    assert result_df.loc[result_df['Goal'] == 'goal_2_score', 'Num_Countries_Above_Mean'].values[0] == 2
+    assert result_df.loc[result_df['Goal'] == 'goal_3_score', 'Num_Countries_Above_Mean'].values[0] == 2
